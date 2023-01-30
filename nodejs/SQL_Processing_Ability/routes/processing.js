6bc8d930575ad525f837ce2566bc2f96f4c0d0c4
WooJJam <jaemin5548@naver.com>
2023-01-30 19:51:12 +0900

    MySQL Sequelize

--- nodejs/SQL_Processing_Ability/routes/processing.js
+++ nodejs/SQL_Processing_Ability/routes/processing.js
@@ -2,9 +2,9 @@ const express = require('express');
 const sqlController = require('../controllers/sql');
 const router = express.Router();
 
-router.post('/testingUser', sqlController.makeTestingUsers);
-router.post('/testingPost', sqlController.makeTestingPosts);
-router.get('/getPosts', sqlController.getPostData);
-router.get('/search', sqlController.searchPostData);
+router.post('/v1/testing/testingUser', sqlController.makeTestingUsers);
+router.post('/v1/testing/testingPost', sqlController.makeTestingPosts);
+router.get('/v1/post/list', sqlController.getPostData);
+router.get('/v1/post/search', sqlController.searchPostData);
 
 module.exports = router;
